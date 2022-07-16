a248f953f7dc50071d4104e30f6a4a9525a9537b
Aditya Ineuron <acd3101282@gmail.com>
2022-07-16 15:27:09 +0530

    update

--- gates.ino
+++ gates.ino
@@ -1,58 +1,99 @@
-#define switch_1 3
-#define switch_2 4
+#define modeButton 2
+#define led1 3
+#define led2 4
+#define led3 5
+#define led4 6
+
+#define input1 7
+#define input2 8
+#define output 9
 
 
-#define andGate 5
-#define orGate 6
-#define xorGate 7
-#define nandGate 8
+int buttonState = 0;
+int lastButtonState = 0;     // previous state of the button
+int counter = 0;
 
-#define outputLED 2
+int outputState = 0;
 
-int input_1 = 0; //Input 1
-int input_2 = 0; //Input 2
 
-int and_gate = 0;
-int or_gate = 0;
-int xor_gate = 0;
-int nand_gate = 0;
+void setup() {
+  // put your setup code here, to run once:
 
-void setup(){
-  pinMode(switch_1,INPUT);
-  pinMode(switch_2,INPUT);
+  pinMode(modeButton,INPUT);
+  pinMode(input1, INPUT);
+  pinMode(input2, INPUT);
+  
+  pinMode(led1,OUTPUT);
+  pinMode(led2,OUTPUT);
+  pinMode(led3,OUTPUT);
+  pinMode(led4,OUTPUT);
 
-  pinMode(andGate, INPUT);
-  pinMode(orGate, INPUT);
-  pinMode(xorGate, INPUT);
-  pinMode(nandGate, INPUT);
-
-  pinMode(outputLED,OUTPUT);
-
+  pinMode(output, OUTPUT);
+  
   Serial.begin(9600);
+
 }
 
+void loop() {
+ 
+  //Reset Values at start
+  digitalWrite(led1, LOW);
+  digitalWrite(led2, LOW);
+  digitalWrite(led3, LOW);
+  digitalWrite(led4, LOW);
 
-void loop(){
-  int output;
-  input_1 = digitalRead(switch_1);
-  input_2 = digitalRead(switch_2);
 
-  and_gate = digitalRead(andGate);
-  or_gate = digitalRead(orGate);
-  xor_gate = digitalRead(xorGate);
-  nand_gate = digitalRead(nandGate);
+  Serial.println("Input 1: ");
+  Serial.println(digitalRead(input1));
 
-  if(and_gate == HIGH) output = input_1 && input_2;
-  if(or_gate == HIGH) output = input_1 || input_2;
-  if(xor_gate == HIGH) output = input_1 ^ input_2;
-  if(nand_gate == HIGH) output = !(input_1 && input_2);
-
-  if(output == HIGH){
-      //Make OUTPUT High
-      digitalWrite(outputLED, HIGH);      
-  }else{
-      digitalWrite(outputLED, LOW);
+  Serial.println("Input 2: ");
+  Serial.println(digitalRead(input2));
+  
+  buttonState = digitalRead(modeButton);
+  
+   // compare the buttonState to its previous state
+  if (buttonState != lastButtonState) {
+    // if the state has changed, increment the counter
+    if (buttonState == HIGH) {
+      // if the current state is HIGH then the button went from off to on:
+      counter++;
+    }
+    
+    // Delay a little bit to avoid bouncing
+    delay(50);
   }
+  // save the current state as the last state, for next time through the loop
+  lastButtonState = buttonState;
 
+  
+  Serial.println("Counter Value: ");
+  Serial.println(counter);
+  
+  switch(counter){
+    case 1:
+     digitalWrite(led1,HIGH);
+     digitalRead(input1) && digitalRead(input2) ? digitalWrite(output,HIGH) : digitalWrite(output,LOW);
+      break;
+    case 2:
+      digitalWrite(led2,HIGH);
+      digitalRead(input1) || digitalRead(input2) ? digitalWrite(output,HIGH) : digitalWrite(output,LOW);
+      break;
+    case 3:
+      digitalWrite(led3,HIGH);
+      digitalRead(input1) ^ digitalRead(input2) ? digitalWrite(output,HIGH) : digitalWrite(output,LOW);
+      break;
+    case 4:
+      digitalWrite(led4,HIGH);
+      ! (digitalRead(input1) && digitalRead(input2)) ? digitalWrite(output,HIGH) : digitalWrite(output,LOW);
+      break;
+    default:
+      counter = 0;
+      break;
+  }
+  
+ 
+  
   delay(100);
+  
+
 }
